2332fb3afa851936f14de72ec493e0494bc63df4
thiagoiferreira <thiagoiferreira@outlook.com>
2021-05-02 23:54:59 -0300

    Creating Conflict and Location classes for multiagent interaction

---
+++ cbs.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(0, '../')
+ 
+
+class Location(object):
+    def __init__(self, x=-1, y=-1):
+        self.x = x
+        self.y = y
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    def __str__(self):
+        return str((self.x, self.y))
+
+
+class Conflict(object):
+    VERTEX = 1
+    EDGE = 2
+    def __init__(self):
+        self.time = -1
+        self.type = -1
+
+        self.agent_1 = ''
+        self.agent_2 = ''
+
+        self.location_1 = Location()
+        self.location_2 = Location()
+
+    def __str__(self):
+        return '(' + str(self.time) + ', ' + self.agent_1 + ', ' + self.agent_2 + \
+             ', '+ str(self.location_1) + ', ' + str(self.location_2) + ')'
+
