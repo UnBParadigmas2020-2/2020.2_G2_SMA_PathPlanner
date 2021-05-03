8a170210273255e8e9ecd8b77871df72c0ec7157
thiagoiferreira <thiagoiferreira@outlook.com>
2021-05-02 23:55:39 -0300

    Adding Constraints, classes vertex constraint, edgeconstraint and general constrainsts

--- cbs.py
+++ cbs.py
@@ -29,3 +29,41 @@ class Conflict(object):
         return '(' + str(self.time) + ', ' + self.agent_1 + ', ' + self.agent_2 + \
              ', '+ str(self.location_1) + ', ' + str(self.location_2) + ')'
 
+
+class VertexConstraint(object):
+    def __init__(self, time, location):
+        self.time = time
+        self.location = location
+
+    def __eq__(self, other):
+        return self.time == other.time and self.location == other.location
+    def __hash__(self):
+        return hash(str(self.time)+str(self.location))
+    def __str__(self):
+        return '(' + str(self.time) + ', '+ str(self.location) + ')'
+
+class EdgeConstraint(object):
+    def __init__(self, time, location_1, location_2):
+        self.time = time
+        self.location_1 = location_1
+        self.location_2 = location_2
+    def __eq__(self, other):
+        return self.time == other.time and self.location_1 == other.location_1 \
+            and self.location_2 == other.location_2
+    def __hash__(self):
+        return hash(str(self.time) + str(self.location_1) + str(self.location_2))
+    def __str__(self):
+        return '(' + str(self.time) + ', '+ str(self.location_1) +', '+ str(self.location_2) + ')'
+
+class Constraints(object):
+    def __init__(self):
+        self.vertex_constraints = set()
+        self.edge_constraints = set()
+
+    def add_constraint(self, other):
+        self.vertex_constraints |= other.vertex_constraints
+        self.edge_constraints |= other.edge_constraints
+
+    def __str__(self):
+        return "VC: " + str([str(vc) for vc in self.vertex_constraints])  + \
+            "EC: " + str([str(ec) for ec in self.edge_constraints])
