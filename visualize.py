220a79fa9b82c78b10ca4cedea6bd40ae8ea292e
dansousamelo <daniel_sousa.unb@eoutlook.com>
2021-05-03 10:43:18 -0300

    :sparkles:making agents

--- visualize.py
+++ visualize.py
@@ -24,19 +24,47 @@ class Animation:
     self.fig = plt.figure(frameon=False, figsize=(4 * aspect, 4))
     self.ax = self.fig.add_subplot(111, aspect='equal')
     self.fig.subplots_adjust(left=0,right=1,bottom=0,top=1, wspace=None, hspace=None)
-    # self.ax.set_frame_on(False)
 
     self.patches = []
     self.artists = []
     self.agents = dict()
     self.agent_names = dict()
-    # create boundary patch
+
     xmin = -0.5
     ymin = -0.5
     xmax = map["map"]["dimensions"][0] - 0.5
     ymax = map["map"]["dimensions"][1] - 0.5
 
-    if __name__ == "__main__":
+    plt.xlim(xmin, xmax)
+    plt.ylim(ymin, ymax)
+
+
+    self.patches.append(Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, facecolor='#9452cc', edgecolor='#9452cc'))
+    for o in map["map"]["obstacles"]:
+      x, y = o[0], o[1]
+      self.patches.append(Rectangle((x - 0.5, y - 0.5), 1, 1, facecolor='#04cc80', edgecolor='#03b270'))
+
+    self.T = 0
+    for d, i in zip(map["agents"], range(0, len(map["agents"]))):
+      self.patches.append(Rectangle((d["goal"][0] - 0.25, d["goal"][1] - 0.25), 0.5, 0.5, facecolor=Colors[1], edgecolor='black', alpha=0.5))
+    for d, i in zip(map["agents"], range(0, len(map["agents"]))):
+      name = d["name"]
+      self.agents[name] = Circle((d["start"][0], d["start"][1]), 0.3, facecolor=Colors[2], edgecolor='black')
+      self.agents[name].original_face_color = Colors[0]
+      self.patches.append(self.agents[name])
+      self.T = max(self.T, schedule["schedule"][name][-1]["t"])
+      self.agent_names[name] = self.ax.text(d["start"][0], d["start"][1], name.replace('agent', ''))
+      self.agent_names[name].set_horizontalalignment('center')
+      self.agent_names[name].set_verticalalignment('center')
+      self.artists.append(self.agent_names[name])
+
+    self.anim = animation.FuncAnimation(self.fig, self.animate_func,
+                               init_func=self.init_func,
+                               frames=int(self.T+1) * 10,
+                               interval=100,
+                               blit=True)
+
+if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("map", help="input file containing map")
   parser.add_argument("schedule", help="schedule for agents")
