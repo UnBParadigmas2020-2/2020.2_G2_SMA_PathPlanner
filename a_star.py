486fd4586d2054aae7b43e977609307907aef41a
Pedro Rodrigues <peddroprp@gmail.com>
2021-05-03 13:23:34 -0300

    Add method to reconstruct path in A Search
    Co-authored-by: Thiago Ferreira <thiagoiferreira@outlook.com>

--- a_star.py
+++ a_star.py
@@ -4,3 +4,12 @@ class AStar():
         self.admissible_heuristic = env.admissible_heuristic
         self.is_at_goal = env.is_at_goal
         self.get_neighbors = env.get_neighborsz8
+
+    def reconstruct_path(self, history_path, current_path):
+        entirety_path = [current_path]
+
+        while current_path in history_path.keys():
+            current_path = history_path[current_path]
+            entirety_path.append(current_path)
+
+        return entirety_path[::-1]
